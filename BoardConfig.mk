59414607c57d62a80da8a130e41518bfca71a239
ShevT <shev_t@mail.ru>
2016-08-17 23:25:10 +0300

    BoardConfig: define KERNEL_IMAGE_NAME

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -46,6 +46,7 @@ TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_KERNEL_CONFIG := m2note_recovery_defconfig
 TARGET_KERNEL_SOURCE := kernel/meizu/m2note
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_OFFSET := 0x00008000
